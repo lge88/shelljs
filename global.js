f59dc9ead88a7385593bfd559d47a687807e21b2
Li Ge <lge@ucsd.edu>
2013-04-30 21:47:19 -0700

    Update at Tue Apr 30 21:47:19 PDT 2013

--- global.js
+++ global.js
@@ -1,3 +1,8 @@
 var shell = require('./shell.js');
 for (var cmd in shell)
   global[cmd] = shell[cmd];
+
+var path = require('path');
+['join', 'resolve', 'relative', 'basename', 'extname', 'normalize'].forEach(function(cmd) {
+  global[cmd] = path[cmd];
+});
